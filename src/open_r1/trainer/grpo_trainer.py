d9ac16b67e4d4949565fee6f9210876b1c58b361
0E0W-AC <564170206@qq.com>
2025-06-09 15:29:15 +0800

    Fix the issue with multi-image inputs of different sizes.
    When multiple images of different sizes are input, the original duplication method causes misalignment in image_grid_thw. The same issue also exists in the dev version of vlmr1.

--- src/open_r1/trainer/grpo_trainer.py
+++ src/open_r1/trainer/grpo_trainer.py
@@ -409,7 +409,7 @@ class Qwen2VLGRPOTrainer(Trainer):
         # Concatenate prompt_mask with completion_mask for logit computation
         attention_mask = torch.cat([prompt_mask, completion_mask], dim=1)  # (B*G, P+C)
         pixel_values = prompt_inputs["pixel_values"].repeat(self.num_generations, 1)
-        image_grid_thw = prompt_inputs["image_grid_thw"].repeat_interleave(self.num_generations, dim=0)
+        image_grid_thw = prompt_inputs["image_grid_thw"].repeat(self.num_generations, dim=0)
 
         per_token_logps = self._get_per_token_logps(model, prompt_completion_ids, attention_mask, pixel_values, image_grid_thw)
         # Get rid of the prompt (-1 because of the shift done in get_per_token_logps)
